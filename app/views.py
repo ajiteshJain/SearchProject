92e0bebe147ea2bff02fe56f6c0f8fa3a8b65a6e
Ajitesh <ajitesh1882@gmail.com>
2014-11-09 18:04:10 -0500

    UI frontend

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,request
 from app import app
+import json
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -15,7 +16,14 @@ def index():
 	        'body': 'The Avengers movie was so cool!' 
 	    }
 	]
-	return render_template("index.html",
+	return render_template("searchEngine.html",
 	                       title='Home',
 	                       user=user,
 	                       posts=posts)
+
+@app.route('/search')
+def search_main():
+	searchString = request.args.get('searchString','')
+	results = ['www.cc.gatech.edu','www.sify.com','www.google.com']#search(searchString)
+	return json.dumps(results)
+
