23fa989ec6cc202780afba63ea94687b90989c4a
Srishti Gupta <srishti.gupta28@gmail.com>
2014-11-17 21:25:16 -0500

    script for searching on eand 2 words

--- app/script/searching.py
+++ app/script/searching.py
@@ -3,8 +3,37 @@ import MySQLdb
 def SearchWord(word):
 	db = MySQLdb.connect(user="root", db = "cs6422" )
 	cur = db.cursor()
-	cur.execute("SELECT * from WordFrequency where Word LIKE '{0}'".format(word))
-	row = cur.fetchone()
-	print cur.rowcount
-	return cur.rowcount
+
+	cur.execute("SELECT TFIDF, URL from WordFrequency where Word = '{0}' ORDER BY TFIDF LIMIT 10".format(word))
+	results = []
+	for i in range(cur.rowcount):
+		row = cur.fetchone()
+		results.append(row[1])
+
+	return results
+
+
+def SearchMultipleWords(query):
+	# summing tf--idf
+	words = query.split()
+	db = MySQLdb.connect(user="root", db = "cs6422" )
+	cur = db.cursor()
+	results = {}
+	for word in words:
+		cur.execute("SELECT TFIDF, URL from WordFrequency where Word = '{0}'".format(word))
+		for i in range(cur.rowcount):
+			row = cur.fetchone()
+			print type(row[0])
+			if row[1] not in results:
+				results[row[1]] = 0
+			if (row[0] is None) or (len(row[0]) == 0):
+				continue
+			else:
+				results[row[1]] += float(row[0])
+
+	sortedResults = sorted(results.items(), key=lambda x: x[1], reverse=True)[:10]
+
+	print sortedResults
+
+	return sortedResults
 
